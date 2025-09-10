58c3b041885ac2026ab47731e1dbebd2016461c7
lerdisalihi <lerdisalihi@users.noreply.github.com>
2025-09-10 19:13:12 +0200

    fix(lambda): update Jest config for ES modules support

--- web/lambda/jest.config.cjs
+++ web/lambda/jest.config.cjs
@@ -2,6 +2,13 @@
 /** @type {import('jest').Config} */
 module.exports = {
   testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.js'],
+  globals: {
+    'ts-jest': {
+      useESM: true
+    }
+  },
+  transform: {},
   collectCoverage: true,
   coverageThreshold: {
     global: { branches: 70, functions: 70, lines: 70, statements: 70 }
