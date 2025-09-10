0f86b2e31c42eccc644fc9104ac5afaf2e1ee8c4
lerdisalihi <lerdisalihi@users.noreply.github.com>
2025-09-10 19:19:44 +0200

    fix(lambda): add Babel dependencies and fix Jest config for ES modules

--- web/lambda/jest.config.cjs
+++ web/lambda/jest.config.cjs
@@ -2,13 +2,11 @@
 /** @type {import('jest').Config} */
 module.exports = {
   testEnvironment: 'node',
-  extensionsToTreatAsEsm: ['.js'],
-  globals: {
-    'ts-jest': {
-      useESM: true
-    }
+  transform: {
+    '^.+\\.js$': ['babel-jest', {
+      presets: [['@babel/preset-env', { targets: { node: 'current' } }]]
+    }]
   },
-  transform: {},
   collectCoverage: true,
   coverageThreshold: {
     global: { branches: 70, functions: 70, lines: 70, statements: 70 }
