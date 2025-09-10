bde4e3e3e56b7c40d8eb6bc2e01630a77abd2fad
Sibora Berisha <siboraberisha@gmail.com>
2025-09-11 00:07:30 +0200

    Fix buildspec to dynamically discover API Gateway URL instead of hardcoding

--- web/static/js/config.js
+++ web/static/js/config.js
@@ -3,7 +3,7 @@
 
 window.API_CONFIG = {
   // This will be replaced during the CI/CD pipeline
-  API_GATEWAY_URL: 'https://sfexvebp8h.execute-api.eu-north-1.amazonaws.com/dev/contact',
+  API_GATEWAY_URL: '{{API_GATEWAY_URL}}',
   
   // Fallback for local development
   FALLBACK_URL: 'https://sfexvebp8h.execute-api.eu-north-1.amazonaws.com/dev/contact'
