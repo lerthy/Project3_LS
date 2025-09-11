aac627f4e74fdb95cda38a7926382c45e5d9ec89
Your Name <you@example.com>
2025-09-11 21:00:27 +0200

    fix: API Gateway CORS - use AWS_PROXY for OPTIONS to let Lambda handle CORS headers

--- infra/main.tf
+++ infra/main.tf
@@ -159,54 +159,17 @@ resource "aws_api_gateway_integration" "lambda_integration_options" {
   rest_api_id = aws_api_gateway_rest_api.contact_api.id
   resource_id = aws_api_gateway_resource.contact_resource.id
   http_method = aws_api_gateway_method.options_contact.http_method
-  type        = "MOCK"
-
-  request_templates = {
-    "application/json" = jsonencode({ statusCode = 200 })
-  }
+  type        = "AWS_PROXY"
+  uri         = aws_lambda_function.contact.invoke_arn
 }
 
-resource "aws_api_gateway_method_response" "options_200" {
-  rest_api_id = aws_api_gateway_rest_api.contact_api.id
-  resource_id = aws_api_gateway_resource.contact_resource.id
-  http_method = aws_api_gateway_method.options_contact.http_method
-  status_code = "200"
-
-  response_models = {
-    "application/json" = "Empty"
-  }
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin"  = true,
-    "method.response.header.Access-Control-Allow-Headers" = true,
-    "method.response.header.Access-Control-Allow-Methods" = true
-  }
-}
-
-resource "aws_api_gateway_integration_response" "options_200" {
-  rest_api_id = aws_api_gateway_rest_api.contact_api.id
-  resource_id = aws_api_gateway_resource.contact_resource.id
-  http_method = aws_api_gateway_method.options_contact.http_method
-  status_code = aws_api_gateway_method_response.options_200.status_code
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin"  = "'*'",
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,Accept,Origin'",
-    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,POST'"
-  }
-
-  response_templates = {
-    "application/json" = ""
-  }
-}
+# Removed method_response and integration_response for OPTIONS since we use AWS_PROXY
 
 resource "aws_api_gateway_deployment" "contact_deployment" {
   rest_api_id = aws_api_gateway_rest_api.contact_api.id
   depends_on = [
     aws_api_gateway_integration.lambda_integration,
-    aws_api_gateway_integration.lambda_integration_options,
-    aws_api_gateway_method_response.options_200,
-    aws_api_gateway_integration_response.options_200
+    aws_api_gateway_integration.lambda_integration_options
   ]
 
   # Force a new deployment when integrations/methods change
